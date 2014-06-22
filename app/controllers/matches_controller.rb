cbc34b167831396135e56681a5af594220017966
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-06-22 15:34:01 -0400

    add real-time update to other views

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,12 @@
 class MatchesController < ApplicationController
   def index
     @matches = Match.all.order(:start_time)
+    @next_match = Match.next_match
+    @recent_match = Match.last_match
+    @current_matches = []
+    @matches.each do |match|
+      @current_matches << match if match.status == "In-progress"
+    end
   end
 
   def show
